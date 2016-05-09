446627e12694f1ccad994894c7d1e9350b8a4847
douxf <douxf@asiainfo.com>
2016-05-09 20:43:17 +0800

    add constructure without topic

--- sdk-mds-srv/src/main/java/com/ai/paas/ipaas/mds/MsgConsumerFactory.java
+++ sdk-mds-srv/src/main/java/com/ai/paas/ipaas/mds/MsgConsumerFactory.java
@@ -1,15 +1,21 @@
 package com.ai.paas.ipaas.mds;
 
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+import java.util.concurrent.ConcurrentHashMap;
+
+import com.ai.paas.ipaas.ccs.constants.ConfigException;
+import com.ai.paas.ipaas.ccs.inner.CCSComponentFactory;
 import com.ai.paas.ipaas.ccs.zookeeper.ZKClient;
 import com.ai.paas.ipaas.ccs.zookeeper.impl.ZKPoolFactory;
 import com.ai.paas.ipaas.mds.impl.consumer.MessageConsumer;
 import com.ai.paas.ipaas.mds.impl.consumer.client.KafkaConfig;
+import com.ai.paas.ipaas.uac.service.UserClientFactory;
+import com.ai.paas.ipaas.uac.vo.AuthDescriptor;
+import com.ai.paas.ipaas.uac.vo.AuthResult;
 import com.ai.paas.ipaas.util.Assert;
 import com.ai.paas.ipaas.util.ResourceUtil;
-
-import java.util.Map;
-import java.util.Properties;
-import java.util.concurrent.ConcurrentHashMap;
 
 public class MsgConsumerFactory {
 	static {
@@ -20,61 +26,94 @@ public class MsgConsumerFactory {
 		// 禁止私有化
 	}
 
+	private static Map<String, IMessageConsumer> consumers = new ConcurrentHashMap<String, IMessageConsumer>();
 	private static Map<String, IMessageConsumer> _consumers = new ConcurrentHashMap<String, IMessageConsumer>();
 
-	/**
-	 * @param props
-	 * @param topic
-	 * @param msgProcessorHandler
-	 * @return
-	 */
-	public static IMessageConsumer getClient(Properties props, String topic,
+	public static IMessageConsumer getClient(AuthDescriptor ad,
+			IMsgProcessorHandler msgProcessorHandler) {
+		IMessageConsumer consumer = null;
+
+		MsgUtil.validate(ad);
+		AuthResult authResult = UserClientFactory.getUserClient().auth(ad);
+		MsgUtil.validateAuthResult(authResult);
+		List<String> children = null;
+		try {
+			children = CCSComponentFactory.getConfigClient(
+					authResult.getConfigAddr(), authResult.getConfigUser(),
+					authResult.getConfigPasswd()).listSubPath(
+					MsgConstant.MSG_CONFIG_ROOT + ad.getServiceId());
+		} catch (ConfigException e) {
+			throw new MessageClientException(
+					"MsgSenderFactory getClient error!", e);
+		}
+		if (null == children || children.size() <= 0) {
+			throw new MessageClientException(
+					"MsgSenderFactory can not get config info for:"
+							+ ad.getServiceId());
+		}
+		String topic = children.get(0);
+		MsgUtil.validateTopic(topic);
+		// 认证通过后，判断是否存在已有实例，有，直接返回
+		if (null != consumers.get(ad.getServiceId() + "_" + topic)) {
+			consumer = consumers.get(ad.getServiceId() + "_" + topic);
+			return consumer;
+		}
+		// 获取内部zk地址后取得该用户的kafka配置信息，返回json
+		// 获取该用户申请的kafka服务配置信息
+		consumer = MsgUtil.instanceConsumer(ad.getServiceId(), authResult,
+				topic, msgProcessorHandler);
+		consumers.put(topic, consumer);
+		return consumer;
+	}
+
+	public static IMessageConsumer getClient(AuthDescriptor ad, String topic,
+			IMsgProcessorHandler msgProcessorHandler) {
+		IMessageConsumer consumer = null;
+
+		MsgUtil.validate(ad);
+		MsgUtil.validateTopic(topic);
+		// 认证通过后，判断是否存在已有实例，有，直接返回
+		if (null != consumers.get(ad.getServiceId() + "_" + topic)) {
+			consumer = consumers.get(ad.getServiceId() + "_" + topic);
+			return consumer;
+		}
+		// 传入用户描述对象，用户认证地址，服务申请号
+		// 进行用户认证
+		AuthResult authResult = UserClientFactory.getUserClient().auth(ad);
+		MsgUtil.validateAuthResult(authResult);
+
+		// 获取内部zk地址后取得该用户的kafka配置信息，返回json
+		// 获取该用户申请的kafka服务配置信息
+		consumer = MsgUtil.instanceConsumer(ad.getServiceId(), authResult,
+				topic, msgProcessorHandler);
+		consumers.put(topic, consumer);
+		return consumer;
+	}
+
+	public static IMessageConsumer getClient(AuthResult authResult,
+			Properties props, String topic,
 			IMsgProcessorHandler msgProcessorHandler) {
 		IMessageConsumer consumer = null;
 		Assert.notNull(props,
 				ResourceUtil.getMessage("com.ai.paas.ipaas.msg.cfg_null"));
 		Assert.notNull(topic,
 				ResourceUtil.getMessage("com.ai.paas.ipaas.msg.topic_null"));
-		if (null != _consumers.get(topic))
+		if (_consumers.containsKey(topic))
 			return _consumers.get(topic);
 		// 这里需要将topic加上
 		props.put("kafka.topic", topic);
 		KafkaConfig kafkaConfig = new KafkaConfig(props);
 		// 开始构建实例
-		String zkAddress = props.getProperty("mds.zookeeper.hosts");
-		if (zkAddress == null || zkAddress.length() == 0) {
-			throw new IllegalArgumentException(
-					"Can not found zookeeper hosts. [mds.zookeeper.hosts] is null ");
-		}
-
-		int timeout = Integer.parseInt(props.getProperty(
-				"mds.zookeeper.timout", "60000"));
-
-		String[] authInfoArray = null;
-		String authInfo = null;
-		try {
-			authInfo = props.getProperty("mds.zookeeper.authInfo");
-
-			if (authInfo != null && authInfo.length() > 0) {
-				String[] authInfoTmp = authInfo.split(":");
-				if (authInfoTmp != null && authInfoTmp.length == 2) {
-					authInfoArray = authInfoTmp;
-				}
-			}
-		} catch (Exception e) {
-			throw new IllegalArgumentException(
-					"Failed to convert [mds.zookeeper.authInfo]:" + authInfo);
-		}
-
 		ZKClient zkClient = null;
 		try {
-			zkClient = ZKPoolFactory.getZKPool(zkAddress, timeout,
-					authInfoArray).getZkClient(zkAddress);
+			zkClient = ZKPoolFactory.getZKPool(authResult.getConfigAddr(),
+					authResult.getConfigUser(), authResult.getConfigPasswd(),
+					60000).getZkClient(authResult.getConfigAddr(),
+					authResult.getConfigUser());
 		} catch (Exception e) {
-			throw new RuntimeException(
-					"Failed to create zkClient during initial message consumer",
-					e);
+			throw new MessageClientException("MessageConsumer init error!", e);
 		}
+
 		consumer = new MessageConsumer(zkClient, kafkaConfig,
 				msgProcessorHandler);
 		_consumers.put(topic, consumer);
--- sdk-mds-srv/src/main/java/com/ai/paas/ipaas/mds/MsgSenderFactory.java
+++ sdk-mds-srv/src/main/java/com/ai/paas/ipaas/mds/MsgSenderFactory.java
@@ -1,10 +1,17 @@
 package com.ai.paas.ipaas.mds;
 
+import com.ai.paas.ipaas.ccs.constants.ConfigException;
+import com.ai.paas.ipaas.ccs.inner.CCSComponentFactory;
 import com.ai.paas.ipaas.mds.impl.sender.MessageSender;
+import com.ai.paas.ipaas.uac.service.UserClientFactory;
+import com.ai.paas.ipaas.uac.vo.AuthDescriptor;
+import com.ai.paas.ipaas.uac.vo.AuthResult;
 import com.ai.paas.ipaas.util.Assert;
 import com.ai.paas.ipaas.util.ResourceUtil;
+
 import kafka.producer.ProducerConfig;
 
+import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 import java.util.concurrent.ConcurrentHashMap;
@@ -19,9 +26,66 @@ public class MsgSenderFactory {
 		// 禁止私有化
 	}
 
+	private static Map<String, IMessageSender> senders = new ConcurrentHashMap<String, IMessageSender>();
 	private static Map<String, IMessageSender> _senders = new ConcurrentHashMap<String, IMessageSender>();
 
-	public static IMessageSender getClient(Properties kafaProps, String topic) {
+	public static IMessageSender getClient(AuthDescriptor ad) {
+		IMessageSender sender = null;
+		MsgUtil.validate(ad);
+		// 进行用户认证
+		AuthResult authResult = UserClientFactory.getUserClient().auth(ad);
+		MsgUtil.validateAuthResult(authResult);
+		// 获取topic
+		List<String> children = null;
+		try {
+			children = CCSComponentFactory.getConfigClient(
+					authResult.getConfigAddr(), authResult.getConfigUser(),
+					authResult.getConfigPasswd()).listSubPath(
+					MsgConstant.MSG_CONFIG_ROOT + ad.getServiceId());
+		} catch (ConfigException e) {
+			throw new MessageClientException(
+					"MsgSenderFactory getClient error!", e);
+		}
+		if (null == children || children.size() <= 0) {
+			throw new MessageClientException(
+					"MsgSenderFactory can not get config info for:"
+							+ ad.getServiceId());
+		}
+		String topic = children.get(0);
+		MsgUtil.validateTopic(topic);
+		// 认证通过后，判断是否存在已有实例，有，直接返回
+		if (null != senders.get(topic)) {
+			sender = senders.get(topic);
+			return sender;
+		}
+		//
+		sender = MsgUtil.instanceSender(ad.getServiceId(), authResult, topic);
+		senders.put(topic, sender);
+		return sender;
+	}
+
+	public static IMessageSender getClient(AuthDescriptor ad, String topic) {
+		IMessageSender sender = null;
+		MsgUtil.validate(ad);
+		MsgUtil.validateTopic(topic);
+		// 认证通过后，判断是否存在已有实例，有，直接返回
+		if (null != senders.get(topic)) {
+			sender = senders.get(topic);
+			return sender;
+		}
+		// 传入用户描述对象，用户认证地址，服务申请号
+		// 进行用户认证
+		AuthResult authResult = UserClientFactory.getUserClient().auth(ad);
+		MsgUtil.validateAuthResult(authResult);
+		// 获取内部zk地址后取得该用户的kafka配置信息，返回json
+		// 获取该用户申请的kafka服务配置信息
+		sender = MsgUtil.instanceSender(ad.getServiceId(), authResult, topic);
+		senders.put(topic, sender);
+		return sender;
+	}
+
+	public static IMessageSender getClient(Properties kafaProps, String userId,
+			String topic) {
 		IMessageSender sender = null;
 		Assert.notNull(kafaProps,
 				ResourceUtil.getMessage("com.ai.paas.ipaas.msg.cfg_null"));
@@ -41,4 +105,5 @@ public class MsgSenderFactory {
 		_senders.put(topic, sender);
 		return sender;
 	}
+
 }
